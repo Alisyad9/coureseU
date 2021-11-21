c55c612b1eec18a31ea6c9d3ac342595a383eebd
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-11-21 18:17:28 +0000

    initial commit

--- codeRegex.js
+++ codeRegex.js
@@ -1,25 +1,35 @@
-var re = /^\w\$/;
+// var re = /^\w\$/;
 
-console.log(re.test("a\n"));
-console.log(re.test("x4c"));
+// console.log(re.test("a\n"));
+// console.log(re.test("x4c"));
 
-re.test();
-console.log(re.test("x38"));
+// re.test();
+// console.log(re.test("x38"));
 
-re.test("x00");
+// re.test("x00");
 
-re.test("x91");
+// re.test("x91");
 
-re.test("x2b");
+// re.test("x2b");
 
-re.test("xff");
+// re.test("xff");
 
-re.test("xfg");
+// re.test("xfg");
 
-re.test("y00");
+// re.test("y00");
 
-re.test("a4");
+// re.test("a4");
 
-re.test("xb3-junk");
-console.log();
-//Expected: false but got: true
+// re.test("xb3-junk");
+// console.log();
+// //Expected: false but got: true
+
+// function reverse(str) {
+//   let reverseWord = "";
+//   for (let index = str.length - 1; index >= 0; index--) {
+//     reverseWord += str[index];
+//   }
+//   return reverseWord;
+// }
+// console.log(reverse("hello"));
+jjj;
