da638df49ae5d5f9b8a8f7cd9cad835798d3bb7a
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-11-29 21:53:10 +0000

    learned about react.createElement('div',{},)

--- react/01-starting-setup/src/App.js
+++ react/01-starting-setup/src/App.js
@@ -1,6 +1,7 @@
 import Test from './components/Test';
 import ExpenseItems from './components/ExpenseItems';
 import Expenses from './components/Expenses';
+import React from 'react';
 
 function App() {
   // const expenses = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
@@ -25,21 +26,13 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
-  return (
-    <div>
-      {/* <h2>Let's get started!</h2>
-      <p>this is visible</p> */}
-      {/* <ExpenseItems
-        expenses={expenses}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      /> */}
-      <Expenses expenses={expenses} />
 
-      {/* <Test /> */}
-    </div>
-  );
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started"),
+  //   React.createElement(Expenses, { expenses: expenses })
+  // );
 }
 
 export default App;
