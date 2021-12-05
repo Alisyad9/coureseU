f465c60a7cc9fa98401361893f65ad7422de4a78
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-12-05 23:34:24 +0000

    added labels and text field type in the form

--- react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,16 @@ import React from 'react'; //not actually required
 
 import '../../css/ExpenseForm.css';
 const ExpenseForm = () => {
-  return <div className="new-expense__controls">expense from</div>;
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__controls">
+          <label>Title</label>
+          <input type="text" className="" />
+        </div>
+      </div>
+    </form>
+  );
 };
 
 export default ExpenseForm;
