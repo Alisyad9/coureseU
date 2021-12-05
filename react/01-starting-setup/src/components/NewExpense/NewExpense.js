a55282b87d02cbefee3a4b843c10a4e52c8e1b5d
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-12-05 23:17:43 +0000

    expense component containing form, in addition to css styling

---
+++ react/01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../css/NewExpense.css';
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <form>yes</form>
+      <button> change tittle</button>
+    </div>
+  );
+};
+
+export default NewExpense;
