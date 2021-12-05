3252d465fa3a0e3bbd90e9e85e748544d562106b
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2021-12-05 23:30:41 +0000

    div containing the styling

--- react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ react/01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,6 +1,8 @@
 import React from 'react'; //not actually required
+
+import '../../css/ExpenseForm.css';
 const ExpenseForm = () => {
-  return;
+  return <div className="new-expense__controls">expense from</div>;
 };
 
 export default ExpenseForm;
