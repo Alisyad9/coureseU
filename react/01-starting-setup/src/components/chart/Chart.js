9b4711a526ec98d27c11188393d0832723c172fc
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-01-23 21:29:48 +0000

    changed the chart.js for testing

--- react/01-starting-setup/src/components/chart/Chart.js
+++ react/01-starting-setup/src/components/chart/Chart.js
@@ -7,18 +7,18 @@ const Chart = (props) => {
   const dataPointsValue = props.dataPoints.map((dataPoint) => dataPoint.value);
   const totalMaximum = Math.max(...dataPointsValue); /// Math.Max takes in an array of [1,2,3,4,5,6,7,8,9]
 
-  return (
-    <div className="chart">
-      {props.dataPoints.map((datapoint) => (
-        <ChartBar
-          key={datapoint.label}
-          value={datapoint.value}
-          maxValue={totalMaximum}
-          label={datapoint.label}
-        />
-      ))}
-    </div>
-  );
+  // return (
+  //   <div className="chart">
+  //     {props.dataPoints.map((datapoint) => (
+  //       <ChartBar
+  //         key={datapoint.label}
+  //         value={datapoint.value}
+  //         maxValue={totalMaximum}
+  //         label={datapoint.label}
+  //       />
+  //     ))}
+  //   </div>
+  // );
 };
 
 export default Chart;
