d6e6cdca50c0a3c379cdbdb62f0237c3fc8cd6e6
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-01-25 23:51:11 +0000

    made a user state for value submitted

--- react/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ react/01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -3,15 +3,22 @@ import React, { useState } from 'react';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
-const CourseInput = props => {
+const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [userSubmitted, setUserSubmitted] = useState(false);
 
-  const goalInputChangeHandler = event => {
+  console.log(userSubmitted);
+
+  const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      console.log('it works fine');
+      return 'lol';
+    }
     props.onAddGoal(enteredValue);
   };
 
