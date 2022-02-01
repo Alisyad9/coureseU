e4dca43da6b315aea24a1ab901d4632c30aa782c
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-02-01 18:36:47 +0000

    using the css modules to style the components of my project

--- react/01-starting-project/src/components/UI/Button/Button.js
+++ react/01-starting-project/src/components/UI/Button/Button.js
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-import './Button.css';
+import styles from './Button.module.css';
 import React from 'react';
 // const Button = styled.button/*css*/ `
 //   font: inherit;
@@ -30,7 +30,7 @@ import React from 'react';
 
 const Button = (props) => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
