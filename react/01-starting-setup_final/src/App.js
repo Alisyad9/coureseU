c0095526c2248e949f161ef847a3cba3399d64b5
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-02-09 23:52:36 +0000

    added user components

--- react/01-starting-setup_final/src/App.js
+++ react/01-starting-setup_final/src/App.js
@@ -1,7 +1,9 @@
+import AddUser from './components/users/AddUser.js';
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <AddUser />
     </div>
   );
 }
---
+++ react/01-starting-setup_final/src/components/users/AddUser.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AddUser = (props) => {
+  return;
+  <form>
+    <label>username</label>
+    <input type="text" />
+  </form>;
+};
+export default AddUser;
