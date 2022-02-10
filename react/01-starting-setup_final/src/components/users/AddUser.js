695002a0884b091d118d63f1d32a23909c6f6e5c
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-02-10 00:19:40 +0000

    form sumbit hander works fine able to had components

--- react/01-starting-setup_final/src/components/users/AddUser.js
+++ react/01-starting-setup_final/src/components/users/AddUser.js
@@ -1,10 +1,37 @@
 import React from 'react';
 
-const AddUser = (props) => {
-  return;
-  <form>
-    <label>username</label>
-    <input type="text" />
-  </form>;
+const AddUser = () => {
+  const [userName, setUserName] = React.useState('');
+  const [age, setAge] = React.useState('');
+  console.log(userName);
+  console.log(age);
+
+  const userNameHandler = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const ageHandler = (event) => {
+    setAge(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    setUserName('');
+    setAge('');
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        id="username"
+        onChange={userNameHandler}
+        value={userName}
+      />
+      <label htmlFor="age">Age</label>
+      <input type="number" id="age" onChange={ageHandler} value={age} />
+      <button type="submit"> submit</button>
+    </form>
+  );
 };
 export default AddUser;
