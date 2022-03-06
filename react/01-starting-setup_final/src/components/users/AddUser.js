60708d042adfcc2c32f0677e24b9c28559d3117c
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-03-06 20:49:31 +0000

    props linked to the card.js for the css styling

--- react/01-starting-setup_final/src/components/users/AddUser.js
+++ react/01-starting-setup_final/src/components/users/AddUser.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from '../style/AddUser.module.css';
+import Card from '../style/Card';
 
 const AddUser = () => {
   const [userName, setUserName] = React.useState('');
@@ -21,29 +22,31 @@ const AddUser = () => {
     setAge('');
   };
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="username">Username</label>
-      <input
-        type="text"
-        id="username"
-        onChange={userNameHandler}
-        value={userName}
-        placeholder="enter username"
-      />
-      <label htmlFor="age">Age</label>
-      <input type="number" id="age" onChange={ageHandler} value={age} />
-      <button type="submit " className={classes.sumbit}>
-        {' '}
-        submit
-      </button>{' '}
-      <span
-        className={classes.tooltip}
-        data-tooltip="Make sure all the fields are filled"
-      >
-        ?
-      </span>
-      <div className={classes.test}>test</div>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          onChange={userNameHandler}
+          value={userName}
+          placeholder="enter username"
+        />
+        <label htmlFor="age">Age</label>
+        <input type="number" id="age" onChange={ageHandler} value={age} />
+        <button type="submit " className={classes.sumbit}>
+          {' '}
+          submit
+        </button>{' '}
+        <span
+          className={classes.tooltip}
+          data-tooltip="Make sure all the fields are filled"
+        >
+          ?
+        </span>
+        <div className={classes.test}>test</div>
+      </form>
+    </Card>
   );
 };
 export default AddUser;
