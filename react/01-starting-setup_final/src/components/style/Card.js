c900f76d5aa12d55163449d477c8b863d428b34d
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-03-06 20:49:09 +0000

    added the props the the class name, it is now working

--- react/01-starting-setup_final/src/components/style/Card.js
+++ react/01-starting-setup_final/src/components/style/Card.js
@@ -4,6 +4,8 @@ import classes from './Card.module.css';
 
 const Card = (props) => {
   console.log(props);
-  return <div className={classes.card}>{props.children}</div>;
+  return (
+    <div className={`${classes.card} ${props.className}`}>{props.children}</div>
+  );
 };
 export default Card;
