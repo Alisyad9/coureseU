b4f7a32179fc44208e2ceaaffb2b3e02834273fc
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-04-05 17:17:55 +0100

    added the Userlist component to the main App

--- react/01-starting-setup_final/src/App.js
+++ react/01-starting-setup_final/src/App.js
@@ -1,13 +1,17 @@
 import AddUser from './components/users/AddUser.js';
 import Card from './components/style/Card.js';
 import classes from './components/style/AddUser.module.css';
+import UserList from './components/users/UserList.js';
 function App() {
   return (
     // <Card className={classes.test}>
 
     //   <div className={classes.test}>hey</div>
     // </Card>
-    <AddUser />
+    <div>
+      <AddUser />
+      <UserList users={[]} />
+    </div>
   );
 }
 
