c7317f3a4110ccab2c208eb983318671f2062e89
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-04-17 03:38:12 +0100

    made a function that constantly updates the to do list

--- react/01-starting-setup_final/src/App.js
+++ react/01-starting-setup_final/src/App.js
@@ -2,15 +2,29 @@ import AddUser from './components/users/AddUser.js';
 import Card from './components/style/Card.js';
 import classes from './components/style/AddUser.module.css';
 import UserList from './components/users/UserList.js';
+import React, { useState } from 'react';
 function App() {
+  const [data, setData] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    //append the new input to the new one
+    setData((prevData) => {
+      return [
+        ...prevData,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     // <Card className={classes.test}>
 
     //   <div className={classes.test}>hey</div>
     // </Card>
+
     <div>
-      <AddUser />
-      <UserList users={[1, 2, 3, 4, 5, 6, 7, 8, 9]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={data} />
     </div>
   );
 }
