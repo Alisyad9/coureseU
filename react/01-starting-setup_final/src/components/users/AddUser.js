d395bacc880ac9bdce7262095287e6e9d8e390fc
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-04-17 04:38:34 +0100

    imported the ErrorModal function, passing the props messege and error

--- react/01-starting-setup_final/src/components/users/AddUser.js
+++ react/01-starting-setup_final/src/components/users/AddUser.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from '../style/AddUser.module.css';
 import Card from '../style/Card';
 import Button from '../style/Button';
+import ErrorModal from '../style/ErrorModal';
 
 const AddUser = (props) => {
   ////////////////user state /////
@@ -32,30 +33,33 @@ const AddUser = (props) => {
     setAge('');
   };
   return (
-    <Card className={classes.input}>
-      <form onSubmit={submitHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          onChange={userNameHandler}
-          value={userName}
-          placeholder="enter username"
-        />
-        <label htmlFor="age">Age</label>
-        <input type="number" id="age" onChange={ageHandler} value={age} />
-        <Button type="submit " className={classes.sumbit}>
-          {' '}
-          submit
-        </Button>{' '}
-        <span
-          className={classes.tooltip}
-          data-tooltip="Make sure all the fields are filled"
-        >
-          ?
-        </span>
-      </form>
-    </Card>
+    <div>
+      <Card className={classes.input}>
+        <form onSubmit={submitHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            onChange={userNameHandler}
+            value={userName}
+            placeholder="enter username"
+          />
+          <label htmlFor="age">Age</label>
+          <input type="number" id="age" onChange={ageHandler} value={age} />
+          <Button type="submit " className={classes.sumbit}>
+            {' '}
+            submit
+          </Button>{' '}
+          <span
+            className={classes.tooltip}
+            data-tooltip="Make sure all the fields are filled"
+          >
+            ?
+          </span>
+        </form>
+      </Card>
+      <ErrorModal lol="Error" message="Something went wrong" />
+    </div>
   );
 };
 export default AddUser;
