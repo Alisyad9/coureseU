cd0290c26808a822b4a4bfd801addca0aeda21ae
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-04-30 20:56:51 +0100

    component change

--- react/01-starting-project-error/src/components/UI/Button/Button.js
+++ react/01-starting-project-error/src/components/UI/Button/Button.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import styles from './Button.module.css';
 
-const Button = props => {
-  return (
-    <button type={props.type} className={styles.button} onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
+const Button = (props) => {
+  // return (
+  //   <button type={props.type} className={styles.button} onClick={props.onClick}>
+  //     {props.children}
+  //   </button>
+  // );
 };
 
 export default Button;
