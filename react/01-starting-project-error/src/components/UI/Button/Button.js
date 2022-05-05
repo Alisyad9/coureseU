bdf676400273d77340dc7641db858c9849277160
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-05-05 17:02:21 +0100

    button

--- react/01-starting-project-error/src/components/UI/Button/Button.js
+++ react/01-starting-project-error/src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ import styles from './Button.module.css';
 const Button = (props) => {
   return (
     <button type={props.type} className={styles.button} onClick={props.onClick}>
-      {props.children}
+      {props.children} this works fine button.js
     </button>
   );
 };
