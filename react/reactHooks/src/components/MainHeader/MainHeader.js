25cdd1327b8ed9da3175c0c16cc0d400b355c0cd
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-05-09 22:11:00 +0100

    added tittle

--- react/reactHooks/src/components/MainHeader/MainHeader.js
+++ react/reactHooks/src/components/MainHeader/MainHeader.js
@@ -6,8 +6,11 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
-      <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
+      <h1>Welcome</h1>
+      <Navigation
+        isLoggedIn={props.isAuthenticated}
+        onLogout={props.onLogout}
+      />
     </header>
   );
 };
