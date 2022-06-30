cd0fac64add6864ac4473e6ea90b5604ccb221cb
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-06-30 17:37:30 +0100

    useContext in used instead of Consumer

--- react/reactHooks/src/components/MainHeader/Navigation.js
+++ react/reactHooks/src/components/MainHeader/Navigation.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../context/auth-context';
 
 import classes from './Navigation.module.css';
 
+// will now be using the useContext hook.
 const Navigation = (props) => {
+  const contextData = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {(contextData) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {contextData.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {contextData.isLoggedIn && (
-                <li>
-                  <a href="/">Welcome Sir/Madam</a>
-                </li>
-              )}
-              {contextData.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    // <AuthContext.Consumer>
+    //   {(contextData) => {
+    //     return (
+    <nav className={classes.nav}>
+      <ul>
+        {contextData.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {contextData.isLoggedIn && (
+          <li>
+            <a href="/">Welcome Sir/Madam</a>
+          </li>
+        )}
+        {contextData.isLoggedIn && (
+          <li>
+            <button onClick={contextData.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
+  //     }}
+  //   </AuthContext.Consumer>
+  // );
 };
 
 export default Navigation;
